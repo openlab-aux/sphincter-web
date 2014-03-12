fa93b9e9b5ba6000e47d3be5ec619c719e725aff
Profpatsch <mail@profpatsch.de>
2014-03-12 01:33:48 +0100

    UI Logic
    Mostly done.

--- src-cljs/sphincter_web/core.cljs
+++ src-cljs/sphincter_web/core.cljs
@@ -1,12 +1,14 @@
 (ns sphincter-web.core
-  (:require-macros [cljs.core.async.macros :refer [go]]
+  (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]]
                    [dommy.macros :refer [sel1 node deftemplate]])
   (:require [goog.events :as events]
             [cljs.core.async :refer [put! chan <!]]
             [dommy.core :as dommy]))
 
+;;;; Helpers
 
 (defn listen [el type]
+  "Adds a listener to element el, returns a channel that fires on event type."
   (let [out (chan)]
     (events/listen el type
       (fn [e] (put! out e)))
@@ -16,8 +18,17 @@
   "Retuns the token if it exists in localStorage, nil otherwise."
   (.getItem js/localStorage "token"))
 
-;;;; Markup
+(defn set-token [token]
+  "Sets the token in localStorage."
+  (.setItem js/localStorage "token" token))
 
+(defn query-sphincter [token type]
+  "Queries sphincter to open/close the lab."
+  :success) ;; TODO
+
+;;;; UI Markup
+
+;; Open/Close layout
 (deftemplate buttons [& {:keys [result]}]
   (case (:type result)
     :success (let [t (case (:action result)
@@ -32,14 +43,47 @@
   [:button#open "Open"]
   [:button#close "Close"])
 
+;; New token layout
 (deftemplate new-token [message]
    [:p message]
    [:input#token-input {:placeholder "Token eingeben"}]
    [:button#done "Fertig"])
 
+;;;; UI Logic
+
+(defn log [obj]
+  (.log js/console obj))
+
+(defn token-ui [parent message]
+  "Asks for a token; saves it once given."
+  (go-loop [mes message]
+           (dommy/replace-contents! parent (new-token mes))
+           (let [button (sel1 :#done)
+                 _ (<! (listen button "click"))
+                 token (.-value (sel1 :#token-input))]
+             (if (= token "")
+               (recur "Ein leerer Token macht keinen Sinn …")
+               (do
+                 (set-token token)
+                 (open-close-ui parent))))))
+
+(defn open-close-ui [parent]
+  "Displays an open and a close button to open/close the hackerspace."
+  (go-loop [result {}]
+           (dommy/replace-contents! parent (buttons :result result))
+           (let [open   (sel1 :#open)
+                 close  (sel1 :#close)
+                 openc  (listen open "click")
+                 closec (listen close "click")
+                 token  (get-token)]
+             (when (not token)
+               (token-ui parent "Bitte gib einen Token ein."))
+             (let [action (alt!
+                            openc  :open
+                            closec :closed)
+                   res (query-sphincter action)]
+               (recur {:type res :action action})))))
+
 ;;;; Main
 
-(defn -main []
-  )
-(dommy/replace-contents! (sel1 :main) (buttons))
-(dommy/replace-contents! (sel1 :main) (buttons :result {:type :failure :action :open}))
+(open-close-ui (sel1 :main))
